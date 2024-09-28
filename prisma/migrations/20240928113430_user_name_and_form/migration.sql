19a50a704de0dbd80eccf5df357de8cc366407cb
jonny paulino <jonnyzico10@gmail.com>
2024-09-28 08:35:28 -0300

    fix: migration

--- prisma/migrations/20240928113430_user_name_and_form/migration.sql
+++ prisma/migrations/20240928113430_user_name_and_form/migration.sql
@@ -2,7 +2,6 @@
   Warnings:
 
   - A unique constraint covering the columns `[username]` on the table `users` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `username` to the `users` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
