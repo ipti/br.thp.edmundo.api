840cb8bd1656ef5906d531b97df23a4bd185604b
Felipe Souza <j.felipe.dev.18@gmail.com>
2026-01-31 02:24:58 -0300

    Revert "feat: add nullable order column to module and backfill existing records"
    This reverts commit 7a3825a55cabe6ccc529738e1dd3d0b6c18a914a.

--- prisma/migrations/20260130201754_add_module_order_nullable/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `module` ADD COLUMN `order` INTEGER NULL;
--- prisma/migrations/20260130202050_backfill_module_order/migration.sql
+++
@@ -1,7 +0,0 @@
-UPDATE module m
-JOIN (
-  SELECT id,
-         ROW_NUMBER() OVER (ORDER BY createdAt) AS new_order
-  FROM module
-) o ON o.id = m.id
-SET m.order = o.new_order;
